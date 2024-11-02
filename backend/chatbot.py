759768072805a1445a271a4dacb4ea992b8bc047
Rudra Vidyasagar <rudravidyasagar@rudras-mbp-2.wireless.rit.edu>
2024-11-02 16:09:38 -0400

    prototype 1

--- backend/chatbot.py
+++ backend/chatbot.py
@@ -7,14 +7,12 @@ app = Flask(__name__)
 # Securely load the API key from the environment
 # set opne ai key as enviorment variable so I dont have to hard code it 
 # i made my api key an enviormental variable so i dont have to hard code it
-#openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.api_key = ("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")
+openai.api_key = os.getenv("OPENAI_API_KEY")
+#openai.api_key = ("sk-proj-NxPprzCYSs_NYxsaIMejN2yUkE3b2p3bsy9YQW08Gp0oxqsSRNMMVdMm20mP20P2uDjC9nKlzrT3BlbkFJWo2dYzx9xM1lauhA5OD3M6A21Ib0J2J91VAebRrgHJ1_eqpPTavcUa2a7mz8ip53K4O95TOhEA_=/usr/bin/env")
 @app.route('/api/chat_with_hendrix', methods=['GET'])
 def chat_with_hendrix():
     try:
         prompt = request.args.get('prompt')
-        print(prompt)
-        print(os.getenv("OPENAI_API_KEY"))
         #accesing librabry 
         response = openai.ChatCompletion.create(
             model="gpt-4-turbo",
